94a59a696df97393134ca4d69aeca3d37404c5ad
StkUp <nycbrown@gmail.com>
2011-08-06 02:19:38 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stkup::Application.config.secret_token = 'b7fd59c1520a59e580cbeae74b87ded5d194cc64d4cfbc25600104fe0d0e4def50b21beb24c8f96da2665a180ae010003362b9b2faf19d4db75fdcf1a190f3b0'
